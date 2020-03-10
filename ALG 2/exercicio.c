5944e4793707e95f36577b4c000ad990b5ba2a97
Gabriel Guimarães Vilas Boas Marin <gabrielvbmarin21@gmail.com>
2020-03-10 10:59:16 -0300

    ALG 2

---
+++ ALG 2/exercicio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Cadastrar
+// Pesquisar
+// Buscar
+
+// NUSP;NOME;SOBRENOME;CURSO;NOTA
+// REGISTAR NO ARQUIVO alunos.dad
+
+int main() {
+
+
+
+    return 0
+}
